73ceac57f57dfb801cfe43ac3f2a8398c3940ecd
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2020-02-18 01:51:11 +0700

    Add Tech Section

---
+++ src/components/Home/Tech.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .tech
+    b-container
+      b-row
+        b-col.col-option(cols="8" offset="2")
+          .list
+            img(src='../../assets/images/zendguard.png' width='200px')
+            span PHP Zend Guard Loader
+          .list
+            img(src='../../assets/images/composer.png' width='200px')
+            span PHP Composer
+          .list
+            img(src='../../assets/images/ion-cube.png' width='200px')
+            span PHP ionCub Loader
+        
+</template>
+
+<script>
+export default {
+  name: "Tech",
+  props: {}
+}
+</script>
