2826095352dbe4c3530bc242298ba28d98381bef
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2020-02-18 01:49:31 +0700

    add setting on App.vue and Main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+Vue.component("font-awesome-icon", FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app")
